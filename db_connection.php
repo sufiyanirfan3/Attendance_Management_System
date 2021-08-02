0804a3f6cce463c462f5dbc6c506454a917345df
sufiyanirfan3 <72559300+sufiyanirfan3@users.noreply.github.com>
2021-08-02 14:02:44 +0500

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,14 @@
 <?php
-$servername="localhost";
-$username="root";
-$password="";
+// $servername="localhost";
+// $username="root";
+// $password="";
+// $database="attendance_management_system";
+
+$servername="localdb";
+$username="azure";
+$password="6#vWHD_$";
 $database="attendance_management_system";
+
 
 $conn=mysqli_connect($servername,$username,$password,$database);
 
