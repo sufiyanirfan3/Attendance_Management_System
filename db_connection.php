c013143c4dab746d992432f44e6fc2a1b8856e19
sufiyanirfan3 <72559300+sufiyanirfan3@users.noreply.github.com>
2021-09-16 22:31:36 +0500

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -4,7 +4,7 @@
 // $password="";
 // $database="attendance_management_system";
 
-$servername="127.0.0.1:49709";
+$servername="127.0.0.1:50850";
 $username="azure";
 $password="6#vWHD_$";
 $database="attendance_management_system";
